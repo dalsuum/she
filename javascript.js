f5f2066eb4cb7a855efbeea346bef278b58be194
dalsuum <dalsuum08@gmail.com>
2021-08-29 16:36:41 +0630

    second add javascript file add for photo show

--- javascript.js
+++ javascript.js
@@ -1,14 +1,40 @@
-let box = 1
- function next() {
- document.querySelector(`#box${box}`).style.display = "none"
- box++; if(box > 3) box = 1
- document.querySelector(`#box${box}`).style.display = "block"
- }
- function prev() {
- document.querySelector(`#box${box}`).style.display = "none"
- box--; if(box < 1) box = 3
- document.querySelector(`#box${box}`).style.display = "block"
- }
- document.querySelector("#next").onclick = next
- document.querySelector("#prev").onclick = prev
- setInterval(next, 5000)
+//  img/gallary2/up (1).jpg
+
+
+
+  let load = 0
+
+  let int = setInterval(countNum, 30)
+
+  function countNum(){
+      load++
+
+      if (load > 18){
+          clearInterval(int)
+      }
+
+    let mainDiv = document.getElementById("main-album");
+    let newDiv = document.createElement("div");
+    let newImg = document.createElement("img");
+
+    let myload = `${load}`
+        
+        newDiv.setAttribute("id", "albumphoto")
+        newImg.setAttribute("src", "img/gallary2/up (" + myload + ").jpg");
+        newDiv.appendChild(newImg);
+
+        mainDiv.appendChild(newDiv);
+}
+      
+   
+
+
+
+
+
+
+      
+      
+
+    
+    
